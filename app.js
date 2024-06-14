4066e295f63d2bf56d92cfe7938f5da851546d1e
VishnuGurrapu <gurrapuvishnu20@gmail.com>
2024-06-14 17:21:01 +0000

    commit message

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+app.get("/", (request, response) => {
+  let date = new date();
+  response.send(
+    `${dateTime.getDate()}-${dateTime.getMonth() + 1}-${dateTime.getFullYear()}`
+  );
+});
+module.exports = app;
