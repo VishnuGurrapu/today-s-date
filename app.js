98eb856ec301f9cc938b8feef736d8ad04d119c7
VishnuGurrapu <gurrapuvishnu20@gmail.com>
2024-06-15 19:24:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/", (request, response) => {
-  let date = new date();
+  let dateTime = new date();
   response.send(
     `${dateTime.getDate()}-${dateTime.getMonth() + 1}-${dateTime.getFullYear()}`
   );
